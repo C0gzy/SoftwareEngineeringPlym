fca8d18faf52b3cc1251ede56b4c2789cc4eb4da
Tom Cogzell <58867012+C0gzy@users.noreply.github.com>
2023-10-19 08:56:19 +0100

    Update Arena.cpp

--- Labs/Arena/Arena/Arena/Arena.cpp
+++ Labs/Arena/Arena/Arena/Arena.cpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
 
@@ -24,53 +25,82 @@ uint64_t	64-bit unsigned integer
 
 
 
-
-
-struct Enemies
-{
+struct Entity {
     string name;
     int8_t health = 0;
+    int8_t minattack = 0;
     int8_t attack = 0;
-    int take_damage(short dmg) {
-        health -= dmg;
-        return health;
+    int Gold = 0;
+
+    void CreateDataEntry(string _n, int _h, int _a, int _ma , int _g) {
+        name = _n;
+        health = _h;
+        attack = _a;
+        minattack = _ma;
+        Gold = _g;
     }
+
+
+    int take_damage(short dmg) {
+        srand((unsigned) time(0));
+
+
+        int Attacked = minattack + (rand() % attack);
+        health -= Attacked;  //dmg;
+        
+        return Attacked;
+    }
+};
+
+struct Enemies : Entity
+{
+    
 
 };
 
 
-string EnemyArray[1][3] = {
-    {"Goblin","10","2"}
+string EnemyArray[1][5] = {
+    {"Goblin","10","1","5","10"}
 };
 
 struct Enemies AllEnemies[5];
 int Choice;
+int8_t Result;
 
-struct PlayerStruct
+struct PlayerStruct : Entity
 {
-    string name;
-    int8_t health = 100;
-    int8_t attack = 5;
-    int take_damage(short dmg) {
-        health -= dmg;
-        return health;
-    }
+    
 };
 
 PlayerStruct Player;
+
+
 
 void Create_Enemies() {
 
     for (int i = 0; i < 1; i++) {
         Enemies Current;
-        Current.name = EnemyArray[i][0];
-        Current.health = stoi(EnemyArray[i][1]);
-        Current.attack = stoi(EnemyArray[i][2]);
-
-
+        Current.CreateDataEntry(EnemyArray[i][0] , stoi(EnemyArray[i][1]) , stoi(EnemyArray[i][3]), stoi(EnemyArray[i][2]), stoi(EnemyArray[i][4]));
         AllEnemies[i] = Current;
     }
 
+}
+
+
+int BattleEnd(Enemies CurrentEnemy) {
+
+    if (CurrentEnemy.health > 0) {
+        cout << "---------------------\n        " << CurrentEnemy.name << "     Won this Battle\n       Better luck next time\n" << "---------------------";
+        return 0;
+    }
+    else
+    {
+        Player.Gold += CurrentEnemy.Gold;
+        cout << "---------------------\n   " << Player.name << " Won this Battle\n     You won " << CurrentEnemy.Gold << " Gold \n Current Gold Amount " << Player.Gold << "\n" << "---------------------";
+        return 1;
+    }
+
+    return 1;
 }
 
 int CreateGameScene() {
@@ -78,6 +108,7 @@ int CreateGameScene() {
 
 
     do {
+        system("cls");
         printf("---------------------\n %s \n Health: %d \n ---------------------\n %s \n Health: %d \n ---------------------\n 1)Attack\n",
             CurrentEnemyData.name.c_str(), CurrentEnemyData.health, Player.name.c_str(), Player.health);
 
@@ -87,11 +118,14 @@ int CreateGameScene() {
         {
         case 1:
 
-
-
-            CurrentEnemyData.take_damage(Player.attack);
-
-
+            
+            
+            system("cls");
+            cout << "----------------------------------------\n" << CurrentEnemyData.name << " attacked for " << Player.take_damage(CurrentEnemyData.attack) << " Damage!\n";
+            Sleep(1000);
+            cout << "----------------------------------------\n" << Player.name << " attacked for " << CurrentEnemyData.take_damage(Player.attack) << " Damage!";
+            Sleep(1000);
+            
 
             break;
         default:
@@ -99,20 +133,23 @@ int CreateGameScene() {
         }
     } while ((Player.health > 0) && (CurrentEnemyData.health > 0));
 
-
-    return 0;
+    return BattleEnd(CurrentEnemyData);
 }
 
 int MainMenu() {
+
+    system("cls");
     cout << "Main Menu: \nSelect Decision \n 1)Arena \n";
     cin >> Choice;
-
+    
     switch (Choice)
     {
     case 1:
-        cout << "Arena";
-        system("cls");
-        CreateGameScene();
+        
+        Result = CreateGameScene();
+        if (Result == 0) return 0;
+        if (Result == 1) MainMenu();
+
         break;
     default:
         break;
@@ -123,6 +160,7 @@ int MainMenu() {
 
 int main()
 {
+    Player.CreateDataEntry("Temp", 100, 5 , 3, 0);
     Create_Enemies();
 
     MainMenu();
