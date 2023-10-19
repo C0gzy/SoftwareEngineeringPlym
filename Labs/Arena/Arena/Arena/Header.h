a7fec26d61967efd92b31cf5d103c33f306555ce
Tom Cogzell <58867012+C0gzy@users.noreply.github.com>
2023-10-19 09:49:19 +0100

    Added logo to header

--- Labs/Arena/Arena/Arena/Header.h
+++ Labs/Arena/Arena/Arena/Header.h
@@ -5,6 +5,25 @@
 using namespace std;
 
 const char* MainLogoText =
-
+"                  AAA               RRRRRRRRRRRRRRRRR   EEEEEEEEEEEEEEEEEEEEEENNNNNNNN        NNNNNNNN               AAA\n"
+"                 A:::A              R::::::::::::::::R  E::::::::::::::::::::EN:::::::N       N::::::N              A:::A\n"
+"                A:::::A             R::::::RRRRRR:::::R E::::::::::::::::::::EN::::::::N      N::::::N             A:::::A\n"
+"               A:::::::A            RR:::::R     R:::::REE::::::EEEEEEEEE::::EN:::::::::N     N::::::N            A:::::::A\n"
+"              A:::::::::A             R::::R     R:::::R  E:::::E       EEEEEEN::::::::::N    N::::::N           A:::::::::A\n"
+"             A:::::A:::::A            R::::R     R:::::R  E:::::E             N:::::::::::N   N::::::N          A:::::A:::::A\n"
+"            A:::::A A:::::A           R::::RRRRRR:::::R   E::::::EEEEEEEEEE   N:::::::N::::N  N::::::N         A:::::A A:::::A\n"
+"           A:::::A   A:::::A          R:::::::::::::RR    E:::::::::::::::E   N::::::N N::::N N::::::N        A:::::A   A:::::A\n"
+"          A:::::A     A:::::A         R::::RRRRRR:::::R   E:::::::::::::::E   N::::::N  N::::N:::::::N       A:::::A     A:::::A\n"
+"         A:::::AAAAAAAAA:::::A        R::::R     R:::::R  E::::::EEEEEEEEEE   N::::::N   N:::::::::::N      A:::::AAAAAAAAA:::::A\n"
+"        A:::::::::::::::::::::A       R::::R     R:::::R  E:::::E             N::::::N    N::::::::::N     A:::::::::::::::::::::A\n"
+"       A:::::AAAAAAAAAAAAA:::::A      R::::R     R:::::R  E:::::E       EEEEEEN::::::N     N:::::::::N    A:::::AAAAAAAAAAAAA:::::A\n"
+"      A:::::A             A:::::A   RR:::::R     R:::::REE::::::EEEEEEEE:::::EN::::::N      N::::::::N   A:::::A             A:::::A\n"
+"     A:::::A               A:::::A  R::::::R     R:::::RE::::::::::::::::::::EN::::::N       N:::::::N  A:::::A               A:::::A\n"
+"    A:::::A                 A:::::A R::::::R     R:::::RE::::::::::::::::::::EN::::::N        N::::::N A:::::A                 A:::::A\n"
+"   AAAAAAA                   AAAAAAARRRRRRRR     RRRRRRREEEEEEEEEEEEEEEEEEEEEENNNNNNNN         NNNNNNNAAAAAAA                   AAAAAAA pre - aplha.0.01 C++ Build\n" //15
+"----------------------------------------------------------------------------------------------------------------------------------------\n"
+"                                                           1)Start\n"
+"                                                           2)resume\n"
+"                                                           3)Exit\n";
 
 ;
